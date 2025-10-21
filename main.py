91fa1c37cf725f6cda67a769516b2151d114302b
Dobernadze <tima7723@gmail.com>
2025-10-21 11:20:00 +0500

    feat: Integrate PostgreSQL to complete Phase I/Week 2 requirements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from loguru import logger # <-- НОВЫЙ ИМПОРТ
+from sqlmodel import create_engine
 
 # Настройка логгера для записи в файл
 # Лог-файл будет создан в папке, из которой запускается Uvicorn (внутри контейнера)
@@ -9,9 +10,27 @@ logger.add("logs/app.log", rotation="10 MB", compression="zip")
 class Settings(BaseSettings):
     secret_key: str = "default-secret-key"
     model_config = SettingsConfigDict() 
+    db_url: str = "postgresql://devuser:devpassword@db/appdb"
 
 settings = Settings()
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    try:
+        # Host: 'db' (имя сервиса в compose)
+        engine = create_engine(settings.db_url) 
+        with engine.connect(): # Пытаемся подключиться
+             logger.info("Successfully connected to the database.")
+    except Exception as e:
+        logger.error(f"Failed to connect to the database: {e}")
+
+
+
+
+
+
+
 
 @app.get("/")
 def read_root():
